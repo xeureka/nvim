7fc67d4284b87959ba188020bed639c786e8b561
Eureka Teklemariam <teklemariameureka@gmail.com>
2026-07-27 14:10:34 +0300

    chore(nvim): disable automatic comment continuation on new lines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,11 +7,9 @@ vim.g.wakatime_StatusLineEnabled = 0
 vim.g.wakatime_StatusLine = ""
 
 -- Undercurl
-
 -- for alacritty
 -- vim.cmd([[let &t_Cs = "\e[4:3m]"]])
 -- vim.cmd([[let &t_Ce = "\e[4:3m]"]])
-
 -- for wezterm
 vim.cmd([[let &t_Cs = "\e[60m"]])
 vim.cmd([[let &t_Ce = "\e[24m"]])
@@ -29,7 +27,17 @@ vim.opt.autoindent = true
 -- Disable automatic comment continuation
 -- r: continue comments when pressing Enter
 -- o: continue comments when using o or O
+-- c: auto-wrap comments using textwidth (also causes forced continuation)
+-- Set on FileType (not just once here) because ftplugins run afterward
+-- and re-add these flags to formatoptions per-buffer.
 vim.opt.formatoptions:remove({ "r", "o" })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "r", "o" })
+  end,
+})
 
 -- Show whitespace characters
 vim.opt.list = true
