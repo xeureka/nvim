6fa704180299b4884599eacbf88b7005e1becf19
Eureka Teklemariam <teklemariameureka@gmail.com>
2026-07-27 14:03:58 +0300

    chore(nvim): disable automatic comment continuation on new lines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,18 +11,27 @@ vim.g.wakatime_StatusLine = ""
 -- for alacritty
 -- vim.cmd([[let &t_Cs = "\e[4:3m]"]])
 -- vim.cmd([[let &t_Ce = "\e[4:3m]"]])
+
 -- for wezterm
 vim.cmd([[let &t_Cs = "\e[60m"]])
 vim.cmd([[let &t_Ce = "\e[24m"]])
+
 vim.opt.termguicolors = true
 vim.g.lazyvim_picker = "telescope"
 
+-- Indentation
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 
+-- Disable automatic comment continuation
+-- r: continue comments when pressing Enter
+-- o: continue comments when using o or O
+vim.opt.formatoptions:remove({ "r", "o" })
+
+-- Show whitespace characters
 vim.opt.list = true
 vim.opt.listchars = {
   space = "·",
